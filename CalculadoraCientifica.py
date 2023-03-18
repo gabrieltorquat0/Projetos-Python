1f7d5c6cc3799fc65db60c91a54e59df0b2a155a
gabrieltorquat0 <gabrielviniciust@hotmail.com>
2023-03-18 03:51:02 -0300

    Concluida a operação que converte decimal em hexadecimal

--- CalculadoraCientifica.py
+++ CalculadoraCientifica.py
@@ -1,6 +1,5 @@
 
-
-
+#Função da calculadora padrão
 def Padrao():
     operação = int(input("Qual operação você quer fazer? \n 1 -> + | 2 -> - | 3 -> x | 4 -> / \n"))
     primeiroNumero = int(input("Digite o primeiro número da operação"))
@@ -15,25 +14,36 @@ def Padrao():
     elif operação == 4:
         print (primeiroNumero / segundoNumero)
 
+#Função da calculadora de programador
 def Programador():
     operação = int(input("O número que você vai passar é? \n 1 -> DECIMAL \n 2 -> HEXADECIMAL \n 3 -> OCTAL \n 4 -> BINÁRIO \n"))
     numero = int(input("Qual é o número? "))
 
+    #Operação 1 transformando decimal em hexadecimal
     if operação == 1:
         hexadecimal = []
-
-        #Transformando decimal em hexadecimal
+        
         while numero >= 1:
             hexando = numero % 16
             numero = int(numero / 16)
-            hexadecimal.append(hexando) 
-        if hexadecimal[0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31, 32] == 10:
-            hexadecimal[] = "A"
         
-        hexadecimal.reverse()
-            
+            if hexando >= 0 and hexando <= 9: 
+                hexadecimal.append(hexando)
+            if hexando == 10:
+                hexadecimal.append("A")
+            if hexando == 11:
+                hexadecimal.append("B") 
+            if hexando == 12:
+                hexadecimal.append("C")
+            if hexando == 13:
+                hexadecimal.append("D")
+            if hexando == 14:
+                hexadecimal.append("E")
+            if hexando == 15:
+                hexadecimal.append("F")
 
-    print(hexadecimal)
+        hexadecimal.reverse()
+        print(hexadecimal)
             
     
 
