163b763cf2435e4fc13cf69b94b40fc0681db007
gabrieltorquat0 <gabrielviniciust@hotmail.com>
2023-04-26 11:34:38 -0300

    Calculadora de programador feita

--- CalculadoraCientifica.py
+++ CalculadoraCientifica.py
@@ -1,4 +1,3 @@
-
 #Função da calculadora padrão
 def Padrao():
     operação = int(input("Qual operação você quer fazer? \n 1 -> + | 2 -> - | 3 -> x | 4 -> / \n"))
@@ -16,144 +15,72 @@ def Padrao():
 
 #Função da calculadora de programador
 def Programador():
-    operação = int(input("O número que você vai passar é? \n 1 -> DECIMAL \n 2 -> HEXADECIMAL \n 3 -> OCTAL \n 4 -> BINÁRIO \n"))
-    numero = input("Qual é o número? ")
-
-    #Operação 1: Transformando numeros decimais
-    if operação == 1:
-        def hexadecimal(numero): 
-            hexadecimal = []
-            
-            while numero >= 1:
-                hexando = numero % 16
-                numero = int(numero / 16)
-            
-                if hexando >= 0 and hexando <= 9: 
-                    hexadecimal.append(hexando)
-                if hexando == 10:
-                    hexadecimal.append("A")
-                if hexando == 11:
-                    hexadecimal.append("B") 
-                if hexando == 12:
-                    hexadecimal.append("C")
-                if hexando == 13:
-                    hexadecimal.append("D")
-                if hexando == 14:
-                    hexadecimal.append("E")
-                if hexando == 15:
-                    hexadecimal.append("F")
-
-            hexadecimal.reverse()
-            print(hexadecimal)
-
-        def octal(numero):
-            octal = []
-            
-            while numero >= 1:
-                octando = numero % 8
-                numero = int(numero / 8) 
-                octal.append(octando)
-
-            octal.reverse()
-            print(octal)
+    tipo_operacao = int(input("O número que você vai passar é? \n 1 -> DECIMAL \n 2 -> HEXADECIMAL \n 3 -> OCTAL \n 4 -> BINÁRIO \n"))
+    
+    def operacao(tipo_operacao):
+        # DECIMAL -> HEXADECIMAL / OCTAL / BINÁRIO
+        if tipo_operacao == 1:
+            numero = input("Digite o número decimal:")
+            decimalParabinario(numero)
+            decimalParahexadecimal(numero)
+            decimalParaoctal(numero)
+        # HEXADECIMAL -> DECIMAL / OCTAL / BINÁRIO
+        if tipo_operacao == 2:
+            numero = input("Digite o número HexaDecimal:")
+            HexaDecimalParaDecimal(numero)
         
-        def binário(numero):
-            binario = []
-            
-            while numero >= 1:
-                binariando = numero % 2
-                numero = int(numero / 2) 
-                binario.append(binariando)
+        # OCTAL -> DECIMAL / HEXADECIMAL / BINÁRIO
+        if tipo_operacao == 3:
+            numero = input("Digite o número OCTAL:")
+            octalParaDecimal(numero)
 
-            binario.reverse()
-            print(binario)
+        # BINÁRIO -> DECIMAL / HEXADECIMAL / OCTAL
+        if tipo_operacao == 4:
+            numero = input("Digite o número BINÁRIO:")
+            binarioParaDecimal(numero)
+    
+    def decimalParahexadecimal(numero):
+        hexadecimal = hex(int(numero)).upper()
 
-        binário(numero)
-        hexadecimal(numero)
-        octal(numero)
+        print("Este número em HexaDecimal é:", hexadecimal[2:])
 
-    #Operação 2: Transformando numeros hexadecimais
-    if operação == 2:
-        def decimal(numero): 
-            decimal = []
-            numeroList = numero.split(",")
-            quantidadesDeNumeros = len(numeroList) - 1
+    def decimalParaoctal(numero):
+        octal = oct(int(numero))
 
-            while quantidadesDeNumeros >= 0:
-                caractereParaConverter = numeroList.pop(quantidadesDeNumeros)
+        print("Este número em octal é:", octal[2:])
 
-                try:
-                    if isinstance(caractereParaConverter, str):
-                        caractereParaConverter = int(caractereParaConverter)
-                except ValueError:
-                    pass
+    def decimalParabinario(numero):
+        binario = bin(int(numero))
 
-                
-                if isinstance(caractereParaConverter, int):
-                    if caractereParaConverter >= 0 and caractereParaConverter <= 9:
-                        caractereConvertido = (16 ** quantidadesDeNumeros) * caractereParaConverter
-                        print(caractereParaConverter)
-                        print(caractereConvertido)
-                        decimal.append(caractereConvertido)
-                        quantidadesDeNumeros = quantidadesDeNumeros - 1
-                if isinstance(caractereParaConverter, str):
-                    if caractereParaConverter == "A":
-                        caractereConvertido = (16 ** quantidadesDeNumeros) * 10
-                        decimal.append(caractereConvertido)
-                        quantidadesDeNumeros = quantidadesDeNumeros - 1
+        print("Este número em binario é:", binario[2:])
 
-                    if caractereParaConverter == "B":
-                        caractereConvertido = (16 ** quantidadesDeNumeros) * 11
-                        decimal.append(caractereConvertido)
-                        quantidadesDeNumeros = quantidadesDeNumeros - 1
 
-                    if caractereParaConverter == "C":
-                        decimal.remove("C")
-                        caractereConvertido = (16 ** quantidadesDeNumeros + 1) * 12
-                        decimal.append(caractereConvertido)
-                        quantidadesDeNumeros = quantidadesDeNumeros - 1
+    def HexaDecimalParaDecimal(numero):
+        decimando = int(numero, 16)
+    
+        print("Este número em decimal é:", decimando)
+        
+        decimalParaoctal(decimando)
+        decimalParabinario(decimando)
 
-                    if caractereParaConverter == "D":
-                        caractereConvertido = (16 ** quantidadesDeNumeros + 1) * 13
-                        decimal.remove("D")
-                        decimal.append(caractereConvertido)
-                        quantidadesDeNumeros = quantidadesDeNumeros - 1
+    def octalParaDecimal(numero):
+        octando = int(numero, 8)
 
-                    if caractereParaConverter == "E":
-                        caractereConvertido = (16 ** quantidadesDeNumeros + 1) * 14
-                        decimal.remove(caractereParaConverter)
-                        decimal.append(caractereConvertido)
-                        quantidadesDeNumeros = quantidadesDeNumeros - 1
+        print("Este número em decimal é:", octando)
 
-                    if caractereParaConverter == "F":
-                        caractereConvertido = (16 ** quantidadesDeNumeros + 1) * 15
-                        decimal.remove("F")
-                        decimal.append(caractereConvertido)
-                        quantidadesDeNumeros = quantidadesDeNumeros - 1
+        decimalParabinario(octando)
+        decimalParahexadecimal(octando)
 
-                #try:
-                    #while True:
-                            #decimal.remove("")
-                #except ValueError:
-                    #pass
+    def binarioParaDecimal(numero):
+        binariando = int(numero, 2)
+        print("Este número em decimal é:", binariando)
 
-            decimal.reverse()
-            print(decimal)
+        decimalParahexadecimal(binariando)
+        decimalParaoctal(binariando)
 
-        decimal(numero)
 
         
 
-
-
-    
-    
-        
-            
-    
-
-
-
-
+    operacao(tipo_operacao)
 Programador()
-    
+        
